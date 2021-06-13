1bd0eadead4d799743d2d1ecdce2d12707f770d7
John Doe <johndoe@example.com>
2021-06-13 22:05:45 +0100

    db

--- bbk-apis/urlstore-api/settings.py
+++ bbk-apis/urlstore-api/settings.py
@@ -1,6 +1,5 @@
-import os
 
 DEBUG = True
 SECRET_KEY = 'hmmzhdjhsdkajhdkad'  # make sure to change this
 
-SQLALCHEMY_DATABASE_URI = 'mysql://root:'+os.environ.get('sql_password')+'@'+os.environ.get('sql_server')+'/'+os.environ.get('sql_db')
+SQLALCHEMY_DATABASE_URI = "sqlite:////database.db"
